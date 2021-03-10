84e94d65deb23cf6c9a184e6be799e0317757ec1
mark@stofloos.nl <mark@stofloos.nl>
2021-03-10 14:34:00 +0100

    Added interview frederik

--- src/components/pages/ExpertInterviewFrederik.tsx
+++ src/components/pages/ExpertInterviewFrederik.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useRef } from "react";
 import FadeIn from "react-fade-in";
-import TableDefault from "../../img/a-TableDefault.jpg";
-import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import Tooltip from "../common/Tooltip";
 import InlineLink from "../common/InlineLink";
+import YouTube from 'react-youtube';
+import Tooltip from "../common/Tooltip";
 
 const scrollToRef = (ref: any, offset: number) =>
     window.scrollTo({ top: ref.current.offsetTop + offset, behavior: "smooth" });
@@ -27,7 +26,10 @@ const Page: React.FC = () => {
         }
         scrollToRef(reference, offset);
     };
-
+    const opts = {
+        height: '650',
+        width: '100%',
+    };
     return (
         <>
             <div className="o-pagenav">
@@ -37,92 +39,63 @@ const Page: React.FC = () => {
                 <div>
                     <button onClick={() => executeScroll(Aanleiding, 120)}>
                         Aanleiding
-          </button>
+                    </button>
                     <button onClick={() => executeScroll(Uitvoering, 120)}>
                         Uitvoering
-          </button>
+                    </button>
                     <button onClick={() => executeScroll(GeheleOnderzoek, 120)}>
                         Gehele onderzoek
-          </button>
+                    </button>
                     <button onClick={() => executeScroll(Conclusie, 120)}>
                         Conclusie
-          </button>
+                    </button>
                 </div>
                 <div className="o-pagenav__fill"></div>
             </div>
             <div className="s-page">
                 <div className="s-page__content">
                     <FadeIn>
-                        <h1>Design specifications</h1>
+                        <h1>Expert interview - Frederik Venneman</h1>
                         <div>
                             <div ref={Aanleiding}>
                                 <h2>Aanleiding</h2>
                                 <p>
-                                    [waarom? is <Tooltip woord="JSON" /> cool ]
-                </p>
+                                    Een Expert interview is uitgevoerd omdat het relevant is om te weten welke componenten- volgens de expert- het hardst nodig zijn om data platform completer te maken.
+                                </p>
+                                <p>
+                                    Frederik Vennemann is de <Tooltip woord="hoofdgebruiker"></Tooltip> van data panel op dit moment. Frederik gaat naar nieuwe klanten legt uit hoe Data panel werkt om het uiteindelijk te verkopen. Ook maakt Frederik voor nieuwe klanten de eerste opzet voor Data panel. Dit proces doet Frederik al ongeveer 1/2e  jaar, en hierdoor heeft hij veel kennis over hoe het programma in elkaar steekt.
+                                </p>
                             </div>
                             <div ref={Uitvoering}>
                                 <h2>Uitvoering</h2>
+                                <p>Frederik Venneman is op 03-04-2021 geïnterviewd. Om alle gestelde vragen en de antwoorden te bekijken, navigeer dan naar het "Gehele Onderzoek" tabje in het menu.</p>
                                 <p>
-                                    [uitvoering]
-                </p>
-                                <ol>
-                                    <li>
-                                        [Lijst item]
-                  </li>
-                                    <li>
-                                        [Lijst item]
-                  </li>
-                                    <li>
-                                        [Lijst item]aA
-                  </li>
-                                </ol>
+                                    <YouTube
+                                        videoId={"kwCizRjU9Ew"}
+                                        opts={opts}
+                                    />
+                                </p>
                             </div>
                             <div ref={GeheleOnderzoek}>
                                 <h2>Gehele Onderzoek</h2>
                                 <p>
-                                    Het gehele onderzoek kan worden bekeken op een extern google docs document. Klik daarvoor <InlineLink text="hier" link="https://docs.google.com/document/d/1Gx76nLg9RF7ighhMSi9AHxYvGc6bT6anEdKVC2TYcrY/edit?usp=sharing" />.
-                </p>
+                                    Het gehele onderzoek kan worden bekeken op een extern google docs document. Klik daarvoor <InlineLink text="hier" link="https://docs.google.com/document/d/13UmE_zY46HrMAtVnO8moyNwPjKErISFjgrVq7ufVIwc/edit?usp=sharing" />.
+                                </p>
                             </div>
                             <div ref={Conclusie}>
                                 <h2>Conclusie</h2>
                                 <p>
-                                    [conclusie]
-                </p>
-                                <ul className="c-double-ul">
-                                    <li>
-                                        [bullet points]
-                  </li>
-                                    <li>
-                                        [bullet points]
-                  </li>
-                                    <li>
-                                        [bullet points]
-                  </li>
-                                </ul>
-
-                                <div className="s-page__content__img__container">
-                                    <div className="s-page__content__img__container__img">
-                                        <h3>Foto</h3>
-                                        <Zoom>
-                                            <img
-                                                className="s-page__content__img__full"
-                                                alt="brainstorm"
-                                                src={TableDefault}
-                                            />
-                                        </Zoom>
-                                    </div>
-                                    <div className="s-page__content__img__container__img">
-                                        <h3>How might we</h3>
-                                        <Zoom>
-                                            <img
-                                                className="s-page__content__img__full"
-                                                alt="How might we"
-                                                src={TableDefault}
-                                            />
-                                        </Zoom>
-                                    </div>
-                                </div>
+                                    Het is duidelijk geworden in welke van de categorieën die gespecificeerd zijn tijdens het <InlineLink link="/page/DesignSpecifications" text="Design specifications" /> onderzoek de meeste werkdruk veroorzaken volgens Frederik Vennemann, <Tooltip woord="hoofdgebruiker" /> van Data panel.
+                                </p>
+                                <p>
+                                    Frederik geeft aan dat het onduidelijk is welke data er beschikbaar is (1:28), hoe die data er uit ziet en vind het frustrerend dat hij steeds in de modellen moet kijken wat de exacte naam is van de data die hij wil gebruiken (5:15). Dit neemt veel tijd in omdat er steeds heen en weer genavigeerd moet worden om namen en labels te checken.
+                                </p>
+                                <p>
+                                    Ook het maken van (grote) formulieren is erg veel moeite (6:45). Dit is volgens Frederik een van de vervelendste dingen om aan te passen/aan te maken op data panel.
+                                </p>
+                                <p>
+                                    De tabs zijn ook een vervelend onderdeel om te configureren geeft Frederik aan.
+                                </p>
                             </div>
                         </div>
                     </FadeIn>
